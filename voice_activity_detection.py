d0f57aface7cfd8f194fa5835d39761c9bfc9217
8Altair <dino.ahmetbegovic@gmail.com>
2026-09-15 13:25:47 +0200

    Added and modified comments and docstring.

--- voice_activity_detection.py
+++ voice_activity_detection.py
@@ -3,7 +3,7 @@ from silero_vad import load_silero_vad, get_speech_timestamps, VADIterator
 from logging_configuration import vad_logger
 
 
-try:    # Load voice activity detection model from Silero only once
+try:    # Load voice activity detection model from Silero only once when this module is imported per Python process
     vad_logger.debug("Loading silero voice activity detection model.")
     voice_detection_model = load_silero_vad()
     vad_logger.debug("Silero model loaded.")
@@ -91,14 +91,14 @@ class VADSession:
         vad_logger.debug("Resetting LiveVADSession.")
         try:
             vad_logger.debug("Resetting VADIterator.")
-            self.vad_iterator.reset_states()
+            self.vad_iterator.reset_states()    # Clears the internal state that VADIterator has accumulated from the audio chunks that are already been fed into it
             vad_logger.debug("VADIterator reset.")
         except Exception:
             vad_logger.exception("Failed to reset VADIterator.")
             raise
 
-        self.speech_buffer.clear()
-        self.currently_collecting = False
+        self.speech_buffer.clear()  # Clear the buffer (list)
+        self.currently_collecting = False   # Stop collecting speech.
 
         vad_logger.debug("LiveVADSession reset.")
 
@@ -119,7 +119,7 @@ class VADSession:
             Raises
             ------
             Exception
-                If audio loading or speech timestamp extraction fails.
+                If speech timestamp extraction fails.
         """
         vad_logger.debug(f"Voice detection from standardized audio array.")
 
@@ -129,7 +129,7 @@ class VADSession:
                 audio,
                 self.model,
                 sampling_rate=self.sampling_rate,
-                return_seconds=True)
+                return_seconds=True)    # Return start/end timestamps in seconds
             vad_logger.debug("Speech timestamps retrieved.")
 
         except Exception:
@@ -165,37 +165,37 @@ class VADSession:
             - Speech accumulation ends after speech-end detection.
         """
         vad_logger.debug(f"Voice detection from a chunk.")
-        window_size_samples = 512 if self.sampling_rate == 16000 else 256
+        window_size_samples = 512 if self.sampling_rate == 16000 else 256   # Required window size for live stream (512 / 16000 = 0.032 s = 32 ms)
         vad_logger.debug(f"Window size samples: {window_size_samples}")
-        for i in range(0, len(audio_chunk), window_size_samples):
-            chunk = audio_chunk[i: i + window_size_samples]
+        for i in range(0, len(audio_chunk), window_size_samples):   # Process the incoming audio chunk in consecutive non-overlapping VAD windows
+            chunk = audio_chunk[i: i + window_size_samples] # Determine a chunk for vad iterator processing based on the provided audio chunk (from the current position with the specified  window size)
             if len(chunk) < window_size_samples:
-                break
+                break   # Stop iterating if the chunk size is smaller than the window size
 
             try:
-                speech: None | dict[str, float] = self.vad_iterator(chunk, return_seconds=True)
+                speech: None | dict[str, float] = self.vad_iterator(chunk, return_seconds=True) # Process the chunk
             except Exception:
                 vad_logger.exception("Failed to iterate over speech data.")
                 raise
 
-            if speech:
+            if speech:  # Check whether vad iterator returned a start/end event
                 vad_logger.debug("Speech is existent.")
-                if "start" in speech.keys():
+                if "start" in speech.keys():    # Check if speech has started
                     vad_logger.debug("Start of the speech detected.")
-                    self.currently_collecting = True
-                    self.speech_buffer = []
-                elif "end" in speech.keys():
+                    self.currently_collecting = True    # Switch to collecting data
+                    self.speech_buffer = [] # Empty the speech buffer so the new speech data is not mixed with the old one
+                elif "end" in speech.keys():    # Check if speech has ended
                     vad_logger.debug("End of the speech detected.")
-                    self.currently_collecting = False
-                    self.speech_buffer.append(chunk)
-                    buffer = self.speech_buffer.copy()
-                    self.speech_buffer = []
+                    self.currently_collecting = False   #  Switch to stop collecting data
+                    self.speech_buffer.append(chunk)    # Append the chunk that caused the end of speech detection
+                    buffer = self.speech_buffer.copy()  # Create a copy of the speech buffer before emptying it
+                    self.speech_buffer = [] # Empty the speech buffer
 
                     vad_logger.debug("Returning current speech buffer.")
-                    return buffer
+                    return buffer   # Return the speech buffer for the next pipeline segment
 
             if self.currently_collecting:
-                self.speech_buffer.append(chunk)
+                self.speech_buffer.append(chunk)    # Append the current speech chunk before processing the next
 
         vad_logger.debug("Returning None voice activity.")
-        return None
+        return None # No completed speech segment to return yet
